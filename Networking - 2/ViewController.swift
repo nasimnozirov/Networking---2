da8be28c92f38b619227d3d8e2f4a8eeebf8e35a
nasim <nasim_nozirov@mail.ru>
2022-06-29 16:20:08 +0300

    Initial Commit

---
+++ Networking - 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Networking - 2
+//
+//  Created by Nasim Nozirov on 29.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
